63578aea662b50c2da0cbe9884f4cc0d929efa29
nadav <nadav@nadav-thinkpad>
2018-05-27 22:27:20 +0300

    Better Interface + Tested On RPi 2

--- src/nadav/tasher/scheduleboard/board/Board.java
+++ src/nadav/tasher/scheduleboard/board/Board.java
@@ -118,54 +118,11 @@ public class Board {
 		mainFrame = new JFrame(programName);
 		mainFrame.setUndecorated(true);
 		mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		scheduleView = new ScheduleView();
+		scheduleView = new ScheduleView(1);
 		birthdayView = new BirthdayView();
-		JScrollPane scheduleScroll = new JScrollPane(scheduleView);
-		scheduleScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scheduleScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
-		scheduleView.setOnUpdated(new Runnable() {
-
-			@Override
-			public void run() {
-				new Thread(new Runnable() {
-
-					@Override
-					public void run() {
-						Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-						while (true) {
-							for (int a = 0; a < scheduleView.getHeight() - screen.height; a += 1) {
-								// System.out.println(a);
-								try {
-									scheduleScroll.getVerticalScrollBar().setValue(a);
-									try {
-										Thread.sleep(10);
-									} catch (InterruptedException e) {
-										e.printStackTrace();
-									}
-								} catch (Exception e) {
-								}
-							}
-							for (int a = scheduleView.getHeight() - screen.height - 1; a > 0; a -= 1) {
-								// System.out.println(a);
-								try {
-									scheduleScroll.getVerticalScrollBar().setValue(a);
-									try {
-										Thread.sleep(10);
-									} catch (InterruptedException e) {
-										e.printStackTrace();
-									}
-								} catch (Exception e) {
-								}
-							}
-						}
-					}
-				}).start();
-			}
-
-		});
 		SwitcherView sv = new SwitcherView();
 		sv.setRepeatType(SwitcherView.INFINITE);
-		sv.addView(scheduleScroll, 40);
+		sv.addView(scheduleView, 40);
 		sv.addView(birthdayView, 1);
 		sv.start();
 		mainFrame.setContentPane(sv);
@@ -193,7 +150,7 @@ public class Board {
 	}
 	
 	private static void refreshOTA() {
-		Checker.checkOTA(settings.optString("maintainer","http://p.nockio.com/handasaim/"), programVersion, new OTAListener() {
+		Checker.checkOTA(settings.optString("maintainer","http://p.nockio.com/handasaim/board/ota"), programVersion, new OTAListener() {
 			
 			@Override
 			public void onOTACheck(boolean updateAvailable) {
--- src/nadav/tasher/scheduleboard/board/ScheduleView.java
+++ src/nadav/tasher/scheduleboard/board/ScheduleView.java
@@ -1,7 +1,6 @@
 package nadav.tasher.scheduleboard.board;
 
 import java.awt.Color;
-import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
@@ -9,8 +8,12 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
 
 import nadav.tasher.scheduleboard.board.appcore.AppCore;
@@ -20,9 +23,11 @@ public class ScheduleView extends JPanel {
 
 	private File schedule;
 	private Runnable doOnUpdate = null;
+	private double screenPrecantage;
 
-	public ScheduleView() {
-//		setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+	public ScheduleView(int screenPrecentage) {
+		// setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		this.screenPrecantage = (double) screenPrecantage / 100;
 		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 		setPreferredSize(new Dimension(screen.width, screen.height));
 		setMinimumSize(getPreferredSize());
@@ -44,24 +49,91 @@ public class ScheduleView extends JPanel {
 		}
 	}
 
+	private static JLabel getLabel(String text) {
+		JLabel label = new JLabel(text);
+		label.setForeground(Color.BLACK);
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setVerticalAlignment(SwingConstants.CENTER);
+		label.setVerticalTextPosition(SwingConstants.CENTER);
+		label.setHorizontalTextPosition(SwingConstants.CENTER);
+		return label;
+	}
+
+	private static JLabel getLabelFormatted(String text) {
+		return getLabel("<html>\u200F" + text + "</html>");
+	}
+
 	private void initSchedule() {
 		removeAll();
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		JPanel schedulePane = new JPanel();
 		ArrayList<Classroom> sc = AppCore.getClasses(AppCore.getSheet(schedule));
 		if (!sc.isEmpty()) {
-			setLayout(new GridLayout(1, sc.size() + 1));
+			JPanel titles = new JPanel();
+			titles.setLayout(new GridLayout(1, sc.size() + 1));
+			titles.setPreferredSize(new Dimension(getWidth(), (int) (screen.height * 0.15)));
+			titles.setBackground(
+					new Color(255, 200 - new Random().nextInt(10) * 4, 200 - new Random().nextInt(10) * 10));
+			for (int t = sc.size() - 1; t >= 0; t--) {
+				JLabel l = getLabel(sc.get(t).name);
+				titles.add(l);
+			}
+			JLabel classes = getLabel("כיתות");
+			classes.setBackground(new Color(200, 200, 200));
+			titles.add(classes);
+			add(titles);
+			schedulePane.setLayout(new GridLayout(1, sc.size() + 1));
 			int longest = 0;
 			for (int c = 0; c < sc.size(); c++) {
 				if (sc.get(c).subjects.size() > longest)
 					longest = sc.get(c).subjects.size();
 			}
-			Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-			setPreferredSize(new Dimension(screen.width, (int) (screen.height * ((double) longest / (double) 6))));
-			setMinimumSize(getPreferredSize());
-			setMaximumSize(getPreferredSize());
-			for (int c = sc.size()-1; c>0; c--) {
-				add(new ClassView(sc.get(c), longest, c));
+			schedulePane.setPreferredSize(
+					new Dimension(getWidth(), (int) (screen.height * ((double) longest / (double) 8))));
+			schedulePane.setMinimumSize(getPreferredSize());
+			schedulePane.setMaximumSize(getPreferredSize());
+			for (int c = sc.size() - 1; c >= 0; c--) {
+				schedulePane.add(new ClassView(sc.get(c), longest, c));
 			}
-			add(new HourView(longest));
+			schedulePane.add(new HourView(longest));
+			JScrollPane scheduleScroll = new JScrollPane(schedulePane);
+			scheduleScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+			scheduleScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+			add(scheduleScroll);
+			new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+					while (true) {
+						for (int a = 0; a < schedulePane.getHeight() - screen.height; a += 150) {
+							// System.out.println(a);
+							try {
+								scheduleScroll.getVerticalScrollBar().setValue(a);
+								try {
+									Thread.sleep(1500);
+								} catch (InterruptedException e) {
+									e.printStackTrace();
+								}
+							} catch (Exception e) {
+							}
+						}
+						for (int a = schedulePane.getHeight() - screen.height - 1; a > 0; a -= 150) {
+							// System.out.println(a);
+							try {
+								scheduleScroll.getVerticalScrollBar().setValue(a);
+								try {
+									Thread.sleep(1500);
+								} catch (InterruptedException e) {
+									e.printStackTrace();
+								}
+							} catch (Exception e) {
+							}
+						}
+					}
+				}
+			}).start();
 		}
 		revalidate();
 		repaint();
@@ -75,31 +147,20 @@ public class ScheduleView extends JPanel {
 		private Color classColor;
 
 		public ClassView(Classroom sc, int hours, int index) {
-			int colorRemover = (index+1) % 16;
+			int colorRemover = (index + 1) % 16;
 			classColor = new Color(200 - colorRemover * 10, 200 - colorRemover * 10, 200);
 			setLayout(new GridLayout(hours + 1, 1));
 			setBackground(classColor);
-			JLabel className = new JLabel(sc.name);
-			className.setHorizontalAlignment(SwingConstants.CENTER);
-			className.setVerticalAlignment(SwingConstants.CENTER);
-			className.setForeground(Color.BLACK);
-			className.setVerticalTextPosition(SwingConstants.CENTER);
-			className.setHorizontalTextPosition(SwingConstants.CENTER);
-			add(className);
 			for (int i = 0; i < hours; i++) {
 				String text = "";
 				if (i < sc.subjects.size()) {
 					text = sc.subjects.get(i).name;
 				}
-				JLabel label = new JLabel("<html>"+"\u200F"+text+"</html>");
-				label.setForeground(Color.BLACK);
-				label.setHorizontalAlignment(SwingConstants.CENTER);
-				label.setVerticalAlignment(SwingConstants.CENTER);
+				JLabel label = getLabelFormatted(text);
+				label.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.BLACK));
 				label.setPreferredSize(new Dimension(getWidth(), label.getHeight()));
 				label.setMinimumSize(label.getPreferredSize());
 				label.setMaximumSize(label.getPreferredSize());
-				label.setVerticalTextPosition(SwingConstants.CENTER);
-				label.setHorizontalTextPosition(SwingConstants.CENTER);
 				add(label);
 			}
 		}
@@ -108,22 +169,11 @@ public class ScheduleView extends JPanel {
 	public static class HourView extends JPanel {
 		public HourView(int hours) {
 			setLayout(new GridLayout(hours + 1, 1));
-			setBackground(new Color(200,200,200));
-			JLabel classes = new JLabel("כיתות");
-			classes.setHorizontalAlignment(SwingConstants.CENTER);
-			classes.setForeground(Color.BLACK);
-			classes.setVerticalAlignment(SwingConstants.CENTER);
-			classes.setVerticalTextPosition(SwingConstants.CENTER);
-			classes.setHorizontalTextPosition(SwingConstants.CENTER);
-			add(classes);
+			setBackground(new Color(200, 200, 200));
 			for (int i = 0; i < hours; i++) {
-				JLabel label = new JLabel(String.valueOf(i));
-				label.setHorizontalAlignment(SwingConstants.CENTER);
-				label.setVerticalAlignment(SwingConstants.CENTER);
-				label.setForeground(Color.BLACK);
-				label.setVerticalTextPosition(SwingConstants.CENTER);
-				label.setHorizontalTextPosition(SwingConstants.CENTER);
-				add(label);
+				JLabel lb=getLabel(String.valueOf(i));
+				lb.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, Color.BLACK));
+				add(lb);
 			}
 		}
 	}
